7da6f417e1985a4ac6833701f145d472ae1eccb5
Railton98 <railtonleal98@gmail.com>
2019-08-29 22:10:39 -0300

    migrations files to Models: User and File and models initial configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'file_id',
+        'profile',
+        'gender',
+        'born_date',
     ];
 
     /**
@@ -36,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
 }
